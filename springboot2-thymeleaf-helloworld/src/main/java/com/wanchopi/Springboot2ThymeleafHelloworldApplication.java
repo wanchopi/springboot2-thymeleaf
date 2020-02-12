de1314bb873d64de5fc8ffcf3a1b011cda7b754f
Usuario <oscarwanchopi@gmail.com>
2020-02-12 20:24:16 +0100

    primera version springboot-thymeleaf

---
+++ springboot2-thymeleaf-helloworld/src/main/java/com/wanchopi/Springboot2ThymeleafHelloworldApplication.java
@@ -0,0 +1,13 @@
+package com.wanchopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2ThymeleafHelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2ThymeleafHelloworldApplication.class, args);
+	}
+
+}
